8d2286292dd4e48a21bb3627af14d5a712679d75
信田 春満 <halhorn@halmidi.com>
2013-09-08 08:36:26 +0900

    add view component

---
+++ HALAnimalSlideShow/ViewController/Component/HALSlideShowViewComponent.h
@@ -0,0 +1,13 @@
+//
+//  HALSlideShowViewComponent.h
+//  HALAnimalSlideShow
+//
+//  Created by 信田 春満 on 2013/09/08.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HALSlideShowViewComponent : UIView
+
+@end
---
+++ HALAnimalSlideShow/ViewController/Component/HALSlideShowViewComponent.m
@@ -0,0 +1,31 @@
+//
+//  HALSlideShowViewComponent.m
+//  HALAnimalSlideShow
+//
+//  Created by 信田 春満 on 2013/09/08.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import "HALSlideShowViewComponent.h"
+
+@implementation HALSlideShowViewComponent
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
